41e7bbb5a616ee538b2df458c1c4da1e982321d2
marcoalonso <marcoalonsorota@hotmail.com>
2022-03-06 22:19:00 -0600

    Empezando

---
+++ QuizzAppp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzAppp
+//
+//  Created by marco rodriguez on 06/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
